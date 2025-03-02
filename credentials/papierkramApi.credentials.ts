e6712e5b54a4d6ed01927c7612c0d648e649a777
Felix Vemmer <felix.vemmer@gmail.com>
2025-03-02 19:48:30 +0100

    Update papierkramApi.credentials.ts with correct structure and authentication

--- credentials/papierkramApi.credentials.ts
+++ credentials/papierkramApi.credentials.ts
@@ -1,17 +1,29 @@
-import { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+import {
+  ICredentialType,
+  INodeProperties,
+} from 'n8n-workflow';
 
 export class PapierkramApi implements ICredentialType {
-	name = 'papierkramApi';
-	displayName = 'Papierkram API';
-	documentationUrl = 'https://felixvemmer.papierkram.de/api/v1/api-docs/api/v1/swagger.json';
-	properties: INodeProperties[] = [
-		{
-			displayName: 'API Token',
-			name: 'apiToken',
-			type: 'string',
-			default: '',
-			required: true,
-			description: 'The API token to access the Papierkram API',
-		},
-	];
+  name = 'papierkramApi';
+  displayName = 'Papierkram API';
+  documentationUrl = 'https://felixvemmer.papierkram.de/api/v1/api-docs/api/v1/swagger.json';
+  properties: INodeProperties[] = [
+    {
+      displayName: 'API Token',
+      name: 'apiKey',
+      type: 'string',
+      default: '',
+      description: 'The API token for accessing the Papierkram API',
+      required: true,
+    },
+  ];
+  authenticate = {
+    type: 'generic',
+    properties: {
+      headers: {
+        Authorization: 'Bearer {{$credentials.apiKey}}',
+      },
+    },
+  };
 }
