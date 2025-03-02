14b92e9b6b9d206875a44642437dee8e74197ef4
Felix Vemmer <felix.vemmer@gmail.com>
2025-03-02 20:40:35 +0100

    feat: Added list and get TimeEntry

--- nodes/Papierkram/Papierkram.node.ts
+++ nodes/Papierkram/Papierkram.node.ts
@@ -58,6 +58,38 @@ export class Papierkram implements BaseOperation {
 						returnData.push(response);
 					}
 				}
+				else if (resource === 'timeEntry') {
+					if (operation === 'list') {
+						endpoint = '/tracker/time_entries';
+						qs.page = this.getNodeParameter('page', i) as number;
+						qs.page_size = this.getNodeParameter('pageSize', i) as number;
+						const orderBy = this.getNodeParameter('orderBy', i) as string;
+						const orderDirection = this.getNodeParameter('orderDirection', i) as string;
+
+						if (orderBy) {
+							qs.order_by = orderBy;
+							qs.order_direction = orderDirection
+						}
+						const response = await this.baseList.run.call(this, {
+							baseUrl,
+							endpoint,
+							credentials,
+							qs
+						});
+						returnData.push(response);
+					}
+
+					if (operation === 'get') {
+						const timeEntryId = this.getNodeParameter('timeEntryId', i) as string;
+						endpoint = `/tracker/time_entries/${timeEntryId}`;
+						const response = await this.baseGet.run.call(this, {
+							baseUrl,
+							endpoint,
+							credentials,
+						});
+						returnData.push(response);
+					}
+				}
 			} catch (error) {
 				// If the error is of type string, convert it to an object with a message property
 				if (typeof error === 'string') {
