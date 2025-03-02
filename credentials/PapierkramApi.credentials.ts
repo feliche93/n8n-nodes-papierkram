9e069986b44b1132f36b3ee81fb5141bd5c7336d
Felix Vemmer <felix.vemmer@gmail.com>
2025-03-02 18:54:40 +0100

    Create credentials file for Papierkram API

---
+++ credentials/PapierkramApi.credentials.ts
@@ -0,0 +1,15 @@
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class PapierkramApi implements ICredentialType {
+  name = 'papierkramApi';
+  displayName = 'Papierkram API';
+  documentationUrl = 'https://felixvemmer.papierkram.de/api/v1/api-docs/api/v1/swagger.json';
+  properties: INodeProperties[] = [
+    {
+      displayName: 'API Token',
+      name: 'apiToken',
+      type': 'string',
+      default: '',
+    },
+  ];
+}
